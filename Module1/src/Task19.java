f592210918ad0fb26afb624e10ec88907bba3f96
Sergey4846 <sergeyruzhankov@gmail.com>
2019-05-06 23:55:40 +0300

    homework 6

---
+++ Module1/src/Task19.java
@@ -0,0 +1,13 @@
+import java.util.regex.Pattern;
+
+
+public class Task19 {
+    public static void main(String[] args) {
+        String text = "Регулярное выражение описывается в программе с помощью строк, подходящих под определённый правилами шаблон.";
+        String text1 = text.trim();
+
+        int a=text1.split("\\s+").length;
+        System.out.println("Количество пробелов в строке: "+a);
+
+    }
+}
